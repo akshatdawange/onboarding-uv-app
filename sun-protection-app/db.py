0136a6407d119883b45d276e0b282ab37fc43cb2
Akshat Dawange <109270129+akshatdawange@users.noreply.github.com>
2026-03-12 23:37:45 +1100

    Delete sun-protection-app directory

--- sun-protection-app/db.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client, Client
-import streamlit as st
-
-def get_supabase() -> Client:
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
